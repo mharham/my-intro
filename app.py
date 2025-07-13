664bc9ba5eb1d2a7ae3492be26d2df871b999b09
mharham <61654904+mharham@users.noreply.github.com>
2025-07-14 00:37:08 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,143 @@
+from pathlib import Path
+
+import streamlit as st
+from PIL import Image
+
+
+# --- PATH SETTINGS ---
+current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
+css_file = current_dir / "styles" / "main.css"
+resume_file = current_dir / "assets" / "CV.pdf"
+profile_pic = current_dir / "assets" / "profile-pic.jpg"
+
+
+# --- GENERAL SETTINGS ---
+PAGE_TITLE = "Digital CV | MH"
+PAGE_ICON = ":wave:"
+NAME = "Muhammad Arham MH"
+DESCRIPTION = """
+Senior Python Programming Developer & Trainer , assisting enterprises by supporting data-driven decision-making.
+"""
+EMAIL = "mharham5@gmail.com"
+SOCIAL_MEDIA = {
+    "YouTube": "https://www.youtube.com/results?search_query=gexton+education+hyderabad",
+    "LinkedIn": "https://linkedin.com",
+    "GitHub": "https://github.com",
+    "Twitter": "https://twitter.com",
+}
+PROJECTS = {
+    "🏆 Sales Dashboard - Comparing sales across three stores": "https://youtu.be/Sb0A9i6d320",
+    "🏆 Income and Expense Tracker - Web app with NoSQL database": "https://youtu.be/3egaMfE9388",
+    "🏆 Desktop Application - Excel2CSV converter with user settings & menubar": "https://youtu.be/LzCfNanQ_9c",
+    "🏆 MyToolBelt - Custom MS Excel add-in to combine Python & Excel": "https://pythonandvba.com/mytoolbelt/",
+}
+
+
+st.set_page_config(page_title=PAGE_TITLE, page_icon=PAGE_ICON)
+
+
+# --- LOAD CSS, PDF & PROFIL PIC ---
+with open(css_file) as f:
+    st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
+with open(resume_file, "rb") as pdf_file:
+    PDFbyte = pdf_file.read()
+profile_pic = Image.open(profile_pic)
+
+
+# --- HERO SECTION ---
+col1, col2 = st.columns(2, gap="small")
+with col1:
+    st.image(profile_pic, width=230)
+
+with col2:
+    st.title(NAME)
+    st.write(DESCRIPTION)
+    st.download_button(
+        label=" 📄 Download Resume",
+        data=PDFbyte,
+        file_name=resume_file.name,
+        mime="application/octet-stream",
+    )
+    st.write("📫", EMAIL)
+
+
+# --- SOCIAL LINKS ---
+st.write('\n')
+cols = st.columns(len(SOCIAL_MEDIA))
+for index, (platform, link) in enumerate(SOCIAL_MEDIA.items()):
+    cols[index].write(f"[{platform}]({link})")
+
+
+# --- EXPERIENCE & QUALIFICATIONS ---
+st.write('\n')
+st.subheader("Experience & Qulifications")
+st.write(
+    """
+- ✔️ Bachelor of Science in IT(Information Technology) from Mehran University of Engineering & Technology Jamshoro
+- ✔️ 2 month Internship of python from Gexton Education HYD
+- ✔️ Good understanding of mathematical principles and their respective applications
+- ✔️ Strong hands on experience and knowledge in Python and Excel
+"""
+)
+
+
+# --- SKILLS ---
+st.write('\n')
+st.subheader("Hard Skills")
+st.write(
+    """
+- 👩‍💻 Programming: Python (Tkinter,Streamlit, Pandas), SQL
+- 📊 Data Visulization: PowerBi, MS Excel, Plotly
+- 📚 Modeling: Logistic regression, linear regression, decition trees
+- 🗄️ Databases: MongoDB, MySQL
+"""
+)
+
+
+# --- WORK HISTORY ---
+st.write('\n')
+st.subheader("Work History")
+st.write("---")
+
+# --- JOB 1
+st.write("🚧", "**Python Developer & Trainner | Gexton Education**")
+st.write("05/2023 - Present")
+st.write(
+    """
+- ► Used PowerBI and SQL to redeﬁne and track KPIs surrounding marketing initiatives, and supplied recommendations to boost landing page conversion rate by 38%
+- ► Led a team of 4 analysts to brainstorm potential marketing and sales improvements, and implemented A/B tests to generate 15% more client leads
+- ► Redesigned data model through iterations that improved predictions by 12%
+"""
+)
+
+# --- JOB 2
+st.write('\n')
+st.write("🚧", "**Computer and Mathematics Teacher | Hidaya Education System**")
+st.write("01/2018 - 08/2022")
+st.write(
+    """
+- ► Developed mathematical models and visual representations to extract valuable insights from complex data sets, enhancing problem-solving skills in various domains.
+- ► Utilized predictive modeling techniques to identify trends and forecast outcomes, showcasing proficiency in data analysis and interpretation.
+- ► Analyzed extensive datasets using statistical methods and computational tools, applying mathematical principles to optimize decision-making processes in real-world scenarios."
+"""
+)
+
+# --- JOB 3
+# st.write('\n')
+# st.write("🚧", "**Data Analyst | Chegg**")
+# st.write("04/2015 - 01/2018")
+# st.write(
+#     """
+# - ► Devised KPIs using SQL across company website in collaboration with cross-functional teams to achieve a 120% jump in organic traﬃc
+# - ► Analyzed, documented, and reported user survey results to improve customer communication processes by 18%
+# - ► Collaborated with analyst team to oversee end-to-end process surrounding customers' return data
+# """
+# )
+
+
+# --- Projects & Accomplishments ---
+st.write('\n')
+st.subheader("Projects & Accomplishments")
+st.write("---")
+for project, link in PROJECTS.items():
+    st.write(f"[{project}]({link})")
